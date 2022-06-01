5e6401adf9648821dfee6ad6174fb05c72127af5
Ksusha <ksushaspring@gmail.com>
2022-06-01 21:56:41 +0300

    game

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
 import sys
-
+from pygame.color import THECOLORS
 
 pygame.init()
+# создание экрана
 screen = pygame.display.set_mode((1200, 800))
+image = pygame.image.load("bin/03.png")
+pygame.display.set_caption("my game")
+bg = pygame.image.load("bin/05.png")
+bg = pygame.transform.scale(bg, (1200, 800))
+
+screen.blit(bg, (20,20))
+image = pygame.transform.scale(image, (230, 230))
+screen.blit(image, (20,20))
+
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.guit()
+            pygame.quit()
             sys.exit()
-
+        pygame.display.flip()
